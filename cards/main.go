5dd0afb682a2e23e7f1f92e4a3a7f3c105c7f234
shockliang <shockwater@gmail.com>
2020-01-04 17:04:41 +0800

    Reading from the hard drive.

--- cards/main.go
+++ cards/main.go
@@ -1,7 +1,9 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my-cards")
+	//cards := newDeck()
+	//cards.saveToFile("my-cards")
+	cards := newDeckFromFile("my-cards")
+	cards.print()
 }
 
