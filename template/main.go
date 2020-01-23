60e7826a6453d9d260cc8067cd2068e815fc5651
shockliang <shockwater@gmail.com>
2020-01-24 02:42:36 +0800

    Passing struct slice into templates.

--- template/main.go
+++ template/main.go
@@ -13,6 +13,17 @@ type sage struct {
 	Motto string
 }
 
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+type items struct {
+	Wisdom    []sage
+	Transport []car
+}
+
 func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
@@ -43,9 +54,28 @@ func main() {
 		Motto: "to overcome evil with good is good, to resist evil by evil is evil.",
 	}
 
+	f150 := car{
+		Manufacturer: "Ford",
+		Model:        "F150",
+		Doors:        2,
+	}
+
+	corolla := car{
+		Manufacturer: "Toyota",
+		Model:        "Corolla",
+		Doors:        4,
+	}
+
 	sages := []sage{buddha, gandhi, mlk, jesus, muhammad}
 
-	err := tpl.Execute(os.Stdout, sages)
+	cars := []car{f150, corolla}
+
+	data := items{
+		Wisdom:    sages,
+		Transport: cars,
+	}
+
+	err := tpl.Execute(os.Stdout, data)
 
 	if err != nil {
 		log.Fatalln(err)
