80c33f310e24be5671d72c6051c95804a76ed8ce
shockliang <shockwater@gmail.com>
2020-01-26 15:21:45 +0800

    Pipelines in templates.

--- template/main.go
+++ template/main.go
@@ -2,40 +2,29 @@ package main
 
 import (
 	"log"
+	"math"
 	"os"
-	"strings"
 	"text/template"
 )
 
 var tpl *template.Template
 
-type sage struct {
-	Name  string
-	Motto string
+func double(x int) int {
+	return x + x
 }
 
-type car struct {
-	Manufacturer string
-	Model        string
-	Doors        int
+func square(x int) float64 {
+	return math.Pow(float64(x), 2)
 }
 
-type items struct {
-	Wisdom    []sage
-	Transport []car
+func sqRoot(x float64) float64 {
+	return math.Sqrt(x)
 }
 
 var fm = template.FuncMap{
-	"uc": strings.ToUpper,
-	"ft": firstThree,
-}
-
-func firstThree(s string) string {
-	s = strings.TrimSpace(s)
-	if len(s) >= 3 {
-		s = s[:3]
-	}
-	return s
+	"fdbl":  double,
+	"fsq":   square,
+	"fsqrt": sqRoot,
 }
 
 func init() {
@@ -43,53 +32,7 @@ func init() {
 }
 
 func main() {
-	buddha := sage{
-		Name:  "Buddha",
-		Motto: "The belief of no beliefs",
-	}
-
-	gandhi := sage{
-		Name:  "Gandhi",
-		Motto: "Be the change",
-	}
-
-	mlk := sage{
-		Name:  "martin Luther King",
-		Motto: "Hatred never ceases with hatred but with love alone is healed.",
-	}
-
-	jesus := sage{
-		Name:  "Jesus",
-		Motto: "Love all",
-	}
-
-	muhammad := sage{
-		Name:  "Muhammad",
-		Motto: "to overcome evil with good is good, to resist evil by evil is evil.",
-	}
-
-	f150 := car{
-		Manufacturer: "Ford",
-		Model:        "F150",
-		Doors:        2,
-	}
-
-	corolla := car{
-		Manufacturer: "Toyota",
-		Model:        "Corolla",
-		Doors:        4,
-	}
-
-	sages := []sage{buddha, gandhi, mlk, jesus, muhammad}
-
-	cars := []car{f150, corolla}
-
-	data := items{
-		Wisdom:    sages,
-		Transport: cars,
-	}
-
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", data)
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 3)
 
 	if err != nil {
 		log.Fatalln(err)
