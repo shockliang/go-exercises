b25b2b8e85027c6192f88a47dc8026bb4b009b2f
shockliang <shockwater@gmail.com>
2020-02-07 09:42:03 +0800

    In-memory database.

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strings"
@@ -26,28 +27,44 @@ func main() {
 }
 
 func handle(conn net.Conn) {
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		ln := strings.ToLower(scanner.Text())
-		bs := []byte(ln)
-		r := rot13(bs)
-		fmt.Fprintf(conn, "%s - %s\n\n",ln, r)
-	}
 	defer conn.Close()
 
-	// Code never go here
-	fmt.Println("Code got here")
-}
+	// instructions
+	io.WriteString(conn, "\nIn-Memory Database\n\n"+
+		"USE:\n"+
+		"SET key value \n"+
+		"GET key \n"+
+		"DEL key \n\n"+
+		"EXAMPLE: \n"+
+		"SET fav chocolate \n"+
+		"GET fav \n\n\n")
 
-func rot13(bs []byte) []byte {
-	var r13 = make([]byte, len(bs))
-	for i, v := range bs {
-		// ascii 97 ~ 122
-		if v <= 109 {
-			r13[i] = v + 13
-		} else {
-			r13[i] = v - 13
+	// read & write
+	data := make(map[string]string)
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fs := strings.Fields(ln)
+		// logic
+		switch fs[0] {
+		case "GET":
+			k:=fs[1]
+			v:=data[k]
+			fmt.Fprintf(conn, "%s\n", v)
+		case "SET":
+			if len(fs) != 3 {
+				fmt.Fprintln(conn, "EXPECTED VALUE")
+				continue
+			}
+			k := fs[1]
+			v := fs[2]
+			data[k] = v
+		case "DEL":
+			k := fs[1]
+			delete(data, k)
+		default:
+			fmt.Fprintln(conn, "INVALID COMMAND")
 		}
+
 	}
-	return  r13
 }
