c91809bc4d11e33756f1359d24466a47435ccf9f
shockliang <shockwater@gmail.com>
2020-02-13 01:36:15 +0800

    Retrieving other request values.

--- http-server/main.go
+++ http-server/main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 type hotdog int
@@ -20,7 +21,23 @@ func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		log.Fatalln(err)
 	}
 
-	tpl.ExecuteTemplate(w, "index.gohtml", r.Form)
+	data := struct {
+		Method        string
+		URL           *url.URL
+		Submissions   url.Values
+		Header        http.Header
+		Host          string
+		ContentLength int64
+	}{
+		r.Method,
+		r.URL,
+		r.Form,
+		r.Header,
+		r.Host,
+		r.ContentLength,
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
 }
 
 func main() {
