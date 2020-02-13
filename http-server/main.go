6432d6d0feb1280d238fbb3cec0aad8923cbc3cf
shockliang <shockwater@gmail.com>
2020-02-14 00:39:11 +0800

    Write header to the response.

--- http-server/main.go
+++ http-server/main.go
@@ -1,43 +1,16 @@
 package main
 
 import (
-	"html/template"
-	"log"
+	"fmt"
 	"net/http"
-	"net/url"
 )
 
 type hotdog int
 
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseFiles("index.gohtml"))
-}
-
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	data := struct {
-		Method        string
-		URL           *url.URL
-		Submissions   url.Values
-		Header        http.Header
-		Host          string
-		ContentLength int64
-	}{
-		r.Method,
-		r.URL,
-		r.Form,
-		r.Header,
-		r.Host,
-		r.ContentLength,
-	}
-
-	tpl.ExecuteTemplate(w, "index.gohtml", data)
+	w.Header().Set("My-Key", "this is my header")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintln(w, "<h1>Any code you want in the func</h1>")
 }
 
 func main() {
