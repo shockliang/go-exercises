9349b32f7dace4f28835ea9308b729dce05a37dc
shockliang <shockwater@gmail.com>
2020-02-27 00:30:58 +0800

    WaitGroup.

--- concurrency/main.go
+++ concurrency/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	fmt.Println("OS\t\t",runtime.GOOS)
@@ -11,17 +14,21 @@ func main() {
 	fmt.Println("CPUs\t\t",runtime.NumCPU())
 	fmt.Println("Goroutines\t",runtime.NumGoroutine())
 
+	wg.Add(2)
 	go foo()
 	bar()
 
 	fmt.Println("CPUs\t\t",runtime.NumCPU())
 	fmt.Println("Goroutines\t",runtime.NumGoroutine())
+
+	wg.Wait()
 }
 
 func foo() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("foo:", i)
 	}
+	wg.Done()
 }
 func bar() {
 	for i := 0; i < 10; i++ {
