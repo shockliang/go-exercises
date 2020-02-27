1edfc100384dad58eb3f6d34d834779ae8ddd10e
shockliang <shockwater@gmail.com>
2020-02-28 01:12:18 +0800

    Channel basic.

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,37 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"sync"
-)
+import "fmt"
 
-var wg sync.WaitGroup
+func doSomething(x int) int {
+	return x * 5
+}
 
 func main() {
-	fmt.Println("OS\t\t",runtime.GOOS)
-	fmt.Println("ARCH\t\t",runtime.GOARCH)
-	fmt.Println("CPUs\t\t",runtime.NumCPU())
-	fmt.Println("Goroutines\t",runtime.NumGoroutine())
-
-	wg.Add(2)
-	go foo()
-	bar()
-
-	fmt.Println("CPUs\t\t",runtime.NumCPU())
-	fmt.Println("Goroutines\t",runtime.NumGoroutine())
-
-	wg.Wait()
-}
-
-func foo() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("foo:", i)
-	}
-	wg.Done()
-}
-func bar() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("bar:", i)
-	}
+	ch := make(chan int)
+	go func() {
+		ch <- doSomething(5)
+	}()
+	fmt.Println(<-ch)
 }
