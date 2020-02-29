d7eda79d4c991dbddb067649468f94df401d7de2
shockliang <shockwater@gmail.com>
2020-02-29 13:24:19 +0800

    Marking race condition case.

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,15 +1,35 @@
 package main
 
-import "fmt"
-
-func doSomething(x int) int {
-	return x * 5
-}
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
 func main() {
-	ch := make(chan int)
-	go func() {
-		ch <- doSomething(5)
-	}()
-	fmt.Println(<-ch)
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	counter := 0
+
+	const gs = 100
+
+	var wg sync.WaitGroup;
+	wg.Add(gs)
+
+	for i := 0; i< gs; i++ {
+		go func() {
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+
 }
