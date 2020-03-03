87fb6c44a58e29a64ec8959a9d87ffcb763101cb
shockliang <shockwater@gmail.com>
2020-03-03 21:31:19 +0800

    Send-only channel

--- channels/main.go
+++ channels/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan int, 1)
+	c := make(chan <- int, 2)
 
 	c <- 42
+	c <- 43
 
-	fmt.Println(<-c)
+	//fmt.Println(<-c)
+	//fmt.Println(<-c)
+	fmt.Printf("%T\n",c)
 }
