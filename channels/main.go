3c7ce44b63e82538a45ef7f0aff6c5c7c57d8d90
shockliang <shockwater@gmail.com>
2020-03-04 13:17:54 +0800

    Directional channel.

--- channels/main.go
+++ channels/main.go
@@ -3,12 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	c := make(<- chan int, 2)
+	c := make(chan int)
+	cr := make (<-chan int)	// receive
+	cs := make(chan <- int) // send
 
-	//c <- 42
-	//c <- 43
-
-	fmt.Println(<-c)
-	fmt.Println(<-c)
 	fmt.Printf("%T\n",c)
+	fmt.Printf("%T\n",cr)
+	fmt.Printf("%T\n",cs)
+
+	// general to specific assigns
+	cr = c
+	cs = c
+
+	fmt.Printf("cr\t%T\n",cr)
+	fmt.Printf("cs\t%T\n",cs)
+
+	// general to specific converts
+	fmt.Printf("c\t%T\n", (<-chan int)(c))
+	fmt.Printf("c\t%T\n", (chan<- int)(c))
 }
