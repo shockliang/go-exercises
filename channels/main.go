958915e9982a08aac9090ec5e91c7dcbf8f8f151
shockliang <shockwater@gmail.com>
2020-03-06 01:03:30 +0800

    Range channel.

--- channels/main.go
+++ channels/main.go
@@ -6,20 +6,17 @@ func main() {
 	c := make(chan int)
 
 	// send
-	go foo(c)
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
 
 	// receive
-	bar(c)
+	for v := range c {
+		fmt.Println(v)
+	}
 
 	fmt.Println("about to exit")
-}
-
-// send
-func foo(c chan <- int){
-	c <- 42
-}
-
-// receive
-func bar(c <-chan int)  {
-	fmt.Println(<-c)
 }
