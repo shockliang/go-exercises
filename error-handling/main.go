c4a6bf7f690841e497edb51bd852cc6fbb4d55ff
shockliang <shockwater@gmail.com>
2020-03-12 09:40:35 +0800

    Printing and logging.

--- error-handling/main.go
+++ error-handling/main.go
@@ -1,26 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	var answer1, answer2, answer3 string
-	fmt.Print("Name: ")
-	_, err := fmt.Scan(&answer1)
+	f, err := os.Create("log.txt")
 	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
+	f2, err := os.Open("no-file.txt")
+	if err != nil {
+		// err happened open no-file.txt: no such file or directory
+		//fmt.Println("err happened", err)
+		// 2020/03/11 22:14:00 err happened open no-file.txt: no such file or directory
+		//log.Println("err happened", err)
+		//log.Fatalln(err)
+		//log.Panic(err)
 		panic(err)
 	}
 
-	fmt.Print("Fav Food: ")
-	_, err = fmt.Scan(&answer2)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Print("Fav Sport: ")
-	_, err = fmt.Scan(&answer3)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(answer1, answer2, answer3)
+	defer f2.Close()
+	fmt.Println("check the log.txt file in the directory")
 }
