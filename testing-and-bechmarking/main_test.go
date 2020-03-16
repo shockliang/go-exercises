5d76143aa2884a0a12bd47cd9a83b7794244f2f0
shockliang <shockwater@gmail.com>
2020-03-17 00:37:16 +0800

    Table testing.

--- testing-and-bechmarking/main_test.go
+++ testing-and-bechmarking/main_test.go
@@ -3,8 +3,23 @@ package main
 import "testing"
 
 func TestMySum(t *testing.T) {
-	x := mySum(2,3)
-	if x != 5 {
-		t.Errorf("Sum 2 and 3 expected to be 5 but got:%v", x)
+	type test struct {
+		data   []int
+		answer int
 	}
+
+	tests := []test{
+		test{[]int{2, 40}, 42},
+		test{[]int{3, 4, 5}, 12},
+		test{[]int{1, 1}, 2},
+		test{[]int{-1, 0, 1}, 0},
+	}
+
+	for _, v := range tests {
+		x := mySum(v.data...)
+		if x != v.answer {
+			t.Error("Expected", v.data, " Got", x)
+		}
+	}
+
 }
