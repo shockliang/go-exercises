efc2f66d595f8d18f1aaf6799b2e7439be30fe07
shockliang <shockwater@gmail.com>
2020-03-20 13:40:35 +0800

    Bechmarking example.

--- testing-and-bechmarking/main_test.go
+++ testing-and-bechmarking/main_test.go
@@ -24,31 +24,3 @@ func BenchmarkGreet(b *testing.B) {
 		saying.Greet("James")
 	}
 }
-
-func ExampleMySum() {
-	fmt.Println(MySum(2, 3))
-	// Output:
-	// 5
-}
-
-func TestMySum(t *testing.T) {
-	type test struct {
-		data   []int
-		answer int
-	}
-
-	tests := []test{
-		test{[]int{2, 40}, 42},
-		test{[]int{3, 4, 5}, 12},
-		test{[]int{1, 1}, 2},
-		test{[]int{-1, 0, 1}, 0},
-	}
-
-	for _, v := range tests {
-		x := MySum(v.data...)
-		if x != v.answer {
-			t.Error("Expected", v.data, " Got", x)
-		}
-	}
-
-}
--- testing-and-bechmarking/mystr/main_test.go
+++ testing-and-bechmarking/mystr/main_test.go
@@ -1,10 +1,44 @@
-// My main package
-package main
+package mystr
 
 import (
 	"fmt"
 	"strings"
+	"testing"
 )
+
+func TestCat(t *testing.T) {
+	s := "Shaken not stirred"
+	xs := strings.Split(s, " ")
+	s =Cat(xs)
+	if s != "Shaken not stirred" {
+		t.Error("got", s, "expected", "Shaken not stirred")
+	}
+}
+
+func TestJoin(t *testing.T) {
+	s := "Shaken not stirred"
+	xs := strings.Split(s, " ")
+	s = Join(xs)
+	if s != "Shaken not stirred" {
+		t.Error("got", s, "expected", "Shaken not stirred")
+	}
+}
+
+func ExampleCat() {
+	s := "Shaken not stirred"
+	xs := strings.Split(s, " ")
+	fmt.Println(Cat(xs))
+	// Output
+	// Shaken not stirred
+}
+
+func ExampleJoin() {
+	s := "Shaken not stirred"
+	xs := strings.Split(s, " ")
+	fmt.Println(Join(xs))
+	// Output
+	// Shaken not stirred
+}
 
 const s = "Contrary to popular belief, " +
 	"Lorem Ipsum is not simply random text. " +
@@ -19,9 +53,17 @@ const s = "Contrary to popular belief, " +
 	"very popular during the Renaissance. " +
 	"The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32."
 
-func main() {
-	xs := strings.Split(s, " ")
-	for _, v := range xs {
-		fmt.Println(v)
+var xs = strings.Split(s, " ")
+
+func BenchmarkCat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Cat(xs)
+	}
+}
+
+func BenchmarkJoin(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		Join(xs)
 	}
 }
