e8ab54f5fb6e07f83ad740adc7da7a9c9bd0a3d7
shockliang <shockwater@gmail.com>
2020-03-22 14:43:50 +0800

    Serving image file.

--- serving-files/main.go
+++ serving-files/main.go
@@ -3,16 +3,29 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", dog)
+	http.HandleFunc("/", smudge)
+	http.HandleFunc("/smudge", smudgeImg)
 	http.ListenAndServe(":8080", nil)
 }
 
-func dog(w http.ResponseWriter, req *http.Request) {
+func smudge(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	io.WriteString(w, `
 		<!-- not serving from our server -->
 		<img src="https://i.insider.com/5df773cefd9db21a1c58b0c4?width=2500&format=jpeg&auto=webp" >`)
+}
+
+func smudgeImg(w http.ResponseWriter, req *http.Request) {
+	f ,err := os.Open("smudge.jpg")
+
+	if err != nil {
+		http.Error(w, "file not found", 404)
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
 }
