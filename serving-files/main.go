196a7eb76b8df443e3ee7b3f277c26cd2b731998
shockliang <shockwater@gmail.com>
2020-03-26 21:42:06 +0800

    Creating a static file server with http.FileServer.

--- serving-files/main.go
+++ serving-files/main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"io"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
-	http.HandleFunc("/smudge", smudge)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
 }
-
-func smudge(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
-	io.WriteString(w, `<img src="/resources/git.png">`)
-}
-
