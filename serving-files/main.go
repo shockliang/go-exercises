f0ac9e12a89695737617506c6276c44f0a7d219b
shockliang <shockwater@gmail.com>
2020-03-27 20:26:34 +0800

    The http.NotFoundHandler.

--- serving-files/main.go
+++ serving-files/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", dog)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL)
+	fmt.Fprintln(w, "go loak at your terminal")
 }
