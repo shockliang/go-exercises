801e5a9192190a486416c4be1d3859abb0b1800d
shockliang <shockwater@gmail.com>
2020-03-30 00:36:05 +0800

    Uploading a file.

--- creating-state/main.go
+++ creating-state/main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", foo)
+	http.Handle("/favicon", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
-	v := req.FormValue("q")
+	var s string
+	fmt.Println(req.Method)
+	if req.Method == http.MethodPost {
+		// open
+		f, h, err := req.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr:", err)
+
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		s = string(bs)
+	}
 	w.Header().Set("Content-type", "text/html; charset=UTF-8")
 	io.WriteString(w, `
-		<form method="POST">
-			<input type="text" name="q">
+		<form method="POST" enctype=multipart/form-data>
+			<input type="file" name="q">
 			<input type="submit">
 		</form>
-		<br>` + v)
+		<br>` + s)
 }
