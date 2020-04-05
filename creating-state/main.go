41e97f2498d5cf50f32088290ed344790039f6ef
shockliang <shockwater@gmail.com>
2020-04-05 15:37:27 +0800

    Writing multiple cookies.

--- creating-state/main.go
+++ creating-state/main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", set)
 	http.HandleFunc("/read", read)
+	http.HandleFunc("/abundance", abundance)
 	http.Handle("/favicon", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
 func set(w http.ResponseWriter, req *http.Request) {
 	http.SetCookie(w, &http.Cookie{
-		Name:"my-cookie",
+		Name:  "my-cookie",
 		Value: "some value",
 	})
 
@@ -23,11 +25,39 @@ func set(w http.ResponseWriter, req *http.Request) {
 }
 
 func read(w http.ResponseWriter, req *http.Request) {
-	c, err := req.Cookie("my-cookie")
+	c1, err := req.Cookie("my-cookie")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNoContent)
-		return
+		log.Println(err)
+	} else {
+		fmt.Fprintln(w, "Your cookie #1:", c1)
 	}
 
-	fmt.Fprintln(w, "Your cookie:", c)
+	c2, err := req.Cookie("general")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Fprintln(w, "Your cookie #2:", c2)
+	}
+
+	c3, err := req.Cookie("specific")
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Fprintln(w, "Your cookie #3:", c3)
+	}
+}
+
+func abundance(w http.ResponseWriter, req *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "general",
+		Value: "general value",
+	})
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "specific",
+		Value: "specific value",
+	})
+
+	fmt.Fprintln(w, "Cookie written - check your browser")
+	fmt.Fprintln(w, "in chrome go to dev tools / application /cookies")
 }
