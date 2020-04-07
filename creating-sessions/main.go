623eb6f1ee5df949d0f8c5f9a651726368f1fa79
shockliang <shockwater@gmail.com>
2020-04-08 00:15:46 +0800

    Creating session.

--- creating-sessions/main.go
+++ creating-sessions/main.go
@@ -1,28 +1,74 @@
 package main
 
 import (
-	"fmt"
 	"github.com/google/uuid"
+	"html/template"
 	"net/http"
 )
 
+type user struct {
+	UserName string
+	First    string
+	Last     string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}      // user id, user
+var dbSessions = map[string]string{} //session id, user id
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
 func main() {
 	http.HandleFunc("/", foo)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.HandleFunc("/bar", bar)
 	http.ListenAndServe(":8080", nil)
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
-	cookie, err := req.Cookie("session")
+	c, err := req.Cookie("session")
 	if err != nil {
-		id := uuid.New()
-
-		cookie = &http.Cookie{
-			Name:     "session",
-			Value:    id.String(),
-			HttpOnly: true,
+		sID := uuid.New()
+		c = &http.Cookie{
+			Name: "session",
+			Value: sID.String(),
 		}
-		http.SetCookie(w, cookie)
+		http.SetCookie(w, c)
 	}
-	fmt.Println(cookie)
+
+	// if the user exists already, get user
+	var u user
+	if un, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[un]
+	}
+
+	// process from submission
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		f := req.FormValue("firstname")
+		l := req.FormValue("lastname")
+		u = user {un, f, l}
+		dbSessions[c.Value] = un
+		dbUsers[un] = u
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	c ,err := req.Cookie("session")
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	un, ok := dbSessions[c.Value]
+	if !ok {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	u := dbUsers[un]
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
 }
