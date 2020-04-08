2c9e0ab238e1e6e767163b69091b916da7f8a8c9
shockliang <shockwater@gmail.com>
2020-04-09 00:51:52 +0800

    Sign-up

---
+++ creating-sessions/session.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/google/uuid"
+	"net/http"
+)
+
+func getUser(w http.ResponseWriter, req *http.Request) user {
+	// get cookie
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID := uuid.New()
+		c = &http.Cookie{Name: "session", Value: sID.String()}
+	}
+	http.SetCookie(w, c)
+
+	var u user
+	if un, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[un]
+	}
+	return u
+}
+
+func alreadyLoggedIn(req *http.Request) bool {
+	c, err := req.Cookie("session")
+	if err != nil {
+		return false
+	}
+	un := dbSessions[c.Value]
+	_, ok := dbUsers[un]
+	return ok
+}
