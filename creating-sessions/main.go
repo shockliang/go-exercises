a53e11ef5114639b75dbf027386da249a588dec3
shockliang <shockwater@gmail.com>
2020-04-11 00:19:05 +0800

    Login.

--- creating-sessions/main.go
+++ creating-sessions/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"html/template"
 	"net/http"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type user struct {
@@ -20,13 +20,50 @@ var dbSessions = map[string]string{} //session id, user id
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
+	bs, _ := bcrypt.GenerateFromPassword([]byte("password", ), bcrypt.MinCost)
+	dbUsers["test@test.com"] = user{"test@test.com", bs, "James", "Bond"}
 }
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/bar", bar)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
 	http.ListenAndServe(":8080", nil)
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+		// is there a username?
+		u, ok := dbUsers[un]
+		if !ok {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+
+		// does the entered password match the stored password?
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+
+		// create session
+		sID := uuid.New()
+		c := &http.Cookie{Name: "session", Value: sID.String()}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "login.gohtml", nil)
 }
 
 func signup(w http.ResponseWriter, req *http.Request) {
@@ -55,7 +92,7 @@ func signup(w http.ResponseWriter, req *http.Request) {
 		dbSessions[c.Value] = un
 
 		// store user in dbUsers
-		bs, err :=bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
+		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
 		if err != nil {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
