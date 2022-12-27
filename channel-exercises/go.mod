5150b022f786d54a0441c7dd086900e70dda6063
shockliang <shockwater@gmail.com>
2022-12-27 22:03:01 +0800

    Channel exercise.

---
+++ channel-exercises/go.mod
@@ -0,0 +1,3 @@
+module channel-exercises
+
+go 1.19
---
+++ channel-exercises/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	DoExit = iota
+	ExitOk
+)
+
+type ControlMsg int
+type Job struct {
+	data int
+}
+type Result struct {
+	result int
+	job    Job
+}
+
+func doubler(jobs <-chan Job, results chan<- Result, control chan ControlMsg) {
+	for {
+		select {
+		case msg := <-control:
+			switch msg {
+			case DoExit:
+				fmt.Println("Exit goroutine")
+				control <- ExitOk
+				return
+			default:
+				panic("unhandled control message")
+			}
+		case job := <-jobs:
+			fmt.Printf("get job %v\n", job.data)
+			results <- Result{result: job.data * 2, job: job}
+			fmt.Printf("job to result done\n")
+
+		}
+
+	}
+}
+
+func main() {
+	jobs := make(chan Job, 50)
+	results := make(chan Result, 50)
+	control := make(chan ControlMsg)
+
+	go doubler(jobs, results, control)
+
+	for i := 0; i < 30; i++ {
+		jobs <- Job{i}
+	}
+
+	for {
+		select {
+		case result := <-results:
+			fmt.Println(result)
+		case <-time.After(500 * time.Millisecond):
+			fmt.Println("timed out")
+			control <- DoExit
+			<-control
+			fmt.Println("program exit")
+			return
+		}
+	}
+}
